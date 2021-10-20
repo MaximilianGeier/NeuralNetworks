732d112e7ead5e52690bd964ecb8d3df14d64910
WEBMaximWEB <maxim.geer@yandex.ru>
2021-10-20 18:03:20 +0500

    Добавьте файлы проекта.

---
+++ NeuralNetworks/NeuralNet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetworks
+{
+    class NeuralNet
+    {
+        public Neuron[] InputLayer { get; set; }
+        public Neuron[][] HiddenLayers { get; set; }
+        public Neuron[] OutputLayer { get; set; }
+
+        public NeuralNet(int inputLayerSize, int hiddenLayerCount, int hiddenLayerSize, int outputLayerSize)
+        {
+            InputLayer = GenerateArray(inputLayerSize);
+            HiddenLayers = new Neuron[hiddenLayerCount][];
+            for (int i = 0; i < hiddenLayerCount; i++)
+                HiddenLayers[i] = GenerateArray(hiddenLayerSize);
+            OutputLayer = GenerateArray(outputLayerSize);
+        }
+
+        private Neuron[] GenerateArray(int size)
+        {
+            Neuron[] output = new Neuron[size];
+            for(int i = 0; i < output.Length; i++)
+                output[i] = new Neuron();
+            return output;
+        }
+    }
+}
---
+++ NeuralNetworks/Neuron.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeuralNetworks
+{
+    class Neuron
+    {
+        public List<Synapse> Synapses = new List<Synapse>();
+        public double Value { get; set; }
+        public void TryAct(double Value) //добавить сюда функцию
+        {
+            Activate(Value);
+        }
+        void Activate(double value)
+        {
+            foreach(Synapse syn in Synapses)
+            {
+                syn.Child.Value += value * syn.Weight;
+            }
+        }
+    }
+
+    class Synapse
+    {
+        public double Weight { get; set; }
+        public Neuron Parent { get; set; }
+        public Neuron Child { get; set; }
+    }
+}
+
